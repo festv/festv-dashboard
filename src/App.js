333adcf5a73bb564eddc982885daaa1579e3e730
Noufal Rahman <noufal24rahman@gmail.com>
2022-10-08 14:12:36 +0530

    registration and login styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import Register from 'pages/register';
 import Login from 'pages/login';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/login',
-      element: <Login />,
-    },
-    {
-      path: '/register',
-      element: <Register />
-    }
-  ])
+  const router = createHashRouter([{
+    path: "/",
+    // element: <h2>Hello World</h2>,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />
+      }
+    ]
+  }])
   return (
     <div className='app'>
       <RouterProvider router={router} />
