b3878094408bc13a5b67aec2b22d5c5416a9ce50
Noufal Rahman <noufal24rahman@gmail.com>
2022-10-08 18:00:45 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
 import Register from 'pages/register';
 import Login from 'pages/login';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
-  const router = createHashRouter([{
-    path: "/",
-    // element: <h2>Hello World</h2>,
-    children: [
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/register',
-        element: <Register />
-      }
-    ]
-  }])
+  const router = createBrowserRouter([
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />
+    }
+  ])
   return (
     <div className='app'>
       <RouterProvider router={router} />
